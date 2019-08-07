bb01f8b3720722c13dec11fb0c2b9fc7384f68b7
Ernesto Jiménez <ernesto@bedu.org>
2019-08-07 17:24:31 -0500

    Removed solution

--- vanilla-app/main.js
+++ vanilla-app/main.js
@@ -5,34 +5,15 @@ let appData = {};
 
 
 const fetchData = async (url) => {
-  try{
-    const r = await fetch(url);
-    return await r.json();
-	} catch(e) {
-    console.error(e);
-	}
 }
 
 const render = (data) => {
-  data.results.map(item => {
-    let character = document.createElement('div');
-    character.innerHTML = `
-    <h1>${item.name}</h1>
-    <p><img src="${item.image}" alt="Picture of ${item.name}"/></p>
-    `;
-    container.appendChild(character);
-  });
 }
 
 const addCharacters = async () => {
-  let data = await fetchData(
-    appData.next || 'https://rickandmortyapi.com/api/character/');
-  appData = {...data.info}
-  render(data)
 }
 
-const init = async () => {
-  addCharacters()
+const init = () => {
 }
 
 init();
